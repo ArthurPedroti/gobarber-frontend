d3050d03ef3fae2bfc53f41a8adf79c554f6fc27
Arthur Pedroti Note <arthurpedroti@gmail.com>
2020-05-24 09:46:30 -0300

    feat: month availability calculation on calendar

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -31,6 +31,8 @@ const AuthProvider: React.FC = ({ children }) => {
     const user = localStorage.getItem('@GoBarber:user');
 
     if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
       return { token, user: JSON.parse(user) };
     }
 
